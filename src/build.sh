41931b03a6e1e4e1dec1a22f37fa83141adfc534
Yri Davies <0xdavy@gmail.com>
2017-12-23 19:47:09 +0100

    Update build script

--- src/build.sh
+++ src/build.sh
@@ -9,23 +9,24 @@ cp -r js/* "${BUILD}/js"
 cp -r css/* "${BUILD}/css"
 cp -r img/* "${BUILD}/img"
 
-echo 'Building index'
-cat "${PARTIAL}/header.html" > "${BUILD}/index.html"
-cat "${HTML}/index.html" >> "${BUILD}/index.html"
-cat "${PARTIAL}/footer.html" >> "${BUILD}/index.html"
-
-sed -i "s/{{{ TITLE }}}/mooidood/g" "${BUILD}/index.html"
-
 PAGES=(
-    "about"
+        "index"
+        "about"
 )
 
 for page in "${PAGES[@]}"
 do
+        # Build page
         echo "Building ${page}"
         cat "${PARTIAL}/header.html" > "${BUILD}/${page}.html"
         cat "${HTML}/${page}.html" >> "${BUILD}/${page}.html"
         cat "${PARTIAL}/footer.html" >> "${BUILD}/${page}.html"
         
-        sed -i "s/{{{ TITLE }}}/mooidood | ${page}/g" "${BUILD}/${page}.html"
+        # Add title
+        if [ ${page} == 'index' ]
+        then    # do index-specific things
+                sed -i "s/{{{ TITLE }}}/mooidood/g" "${BUILD}/${page}.html"
+        else
+                sed -i "s/{{{ TITLE }}}/mooidood | ${page}/g" "${BUILD}/${page}.html"
+        fi
 done
