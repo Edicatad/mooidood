b48adc27cc4b7db611215b10f0517cc408f0a886
Yri Davies <0xdavy@gmail.com>
2018-01-02 17:58:19 +0100

    Implement loader overlay

--- src/js/func.js
+++ src/js/func.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document ready!");
+    document.getElementById("loader").classList.add("hidden");
+});
+
 function expandMenu() {
-        document.getElementById("text").classList.add("visible");
+    document.getElementById("text").classList.add("visible");
 }
