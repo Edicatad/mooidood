9b177ede7a873994f47a0659a7cf8aea9160220b
Yri Davies <0xdavy@gmail.com>
2018-01-02 17:57:48 +0100

    Fix mac-specific sed error

--- src/build.sh
+++ src/build.sh
@@ -10,23 +10,24 @@ cp -r css/* "${BUILD}/css"
 cp -r img/* "${BUILD}/img"
 
 PAGES=(
-        "index"
-        "about"
+"index"
+"about"
 )
 
 for page in "${PAGES[@]}"
 do
-        # Build page
-        echo "Building ${page}"
-        cat "${PARTIAL}/header.html" > "${BUILD}/${page}.html"
-        cat "${HTML}/${page}.html" >> "${BUILD}/${page}.html"
-        cat "${PARTIAL}/footer.html" >> "${BUILD}/${page}.html"
-        
-        # Add title
-        if [ ${page} == 'index' ]
-        then    # do index-specific things
-                sed -i "s/{{{ TITLE }}}/mooidood/g" "${BUILD}/${page}.html"
-        else
-                sed -i "s/{{{ TITLE }}}/mooidood | ${page}/g" "${BUILD}/${page}.html"
-        fi
+    # Build page
+    echo "Building ${page}"
+    cat "${PARTIAL}/header.html" > "${BUILD}/${page}.html"
+    cat "${HTML}/${page}.html" >> "${BUILD}/${page}.html"
+    cat "${PARTIAL}/footer.html" >> "${BUILD}/${page}.html"
+
+    # Add title
+    if [ ${page} == 'index' ]
+    then    # do index-specific things
+        echo "  Performing index-specific actions"
+        sed -i '' -e "s/{{{ TITLE }}}/mooidood/g" "${BUILD}/${page}.html"
+    else
+        sed -i '' -e "s/{{{ TITLE }}}/mooidood | ${page}/g" "${BUILD}/${page}.html"
+    fi
 done
